9bd0b5fdc027a654cc4f1b9477302722f61db36c
sarath <https://github.com/sarathrj10>
2020-06-17 17:25:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,41 @@
+//background music
+bgplay();
+// screen selector
+const frontscreen=document.getElementById('frontpage');
+ const rulescreen=document.getElementById('gamerules');
+const gamescreen=document.getElementById('gamepage');
+//setting current front screen
+ rulescreen.style.display='none';
+ gamescreen.style.display='none';
 //backgroung music
 function bgplay(){
     let bgsound=new Audio('./Audio/bgmusic.mp3');
-    bgsound.play();
+    bgsound.loop = true;
+    bgsound.play();    
     }
-// bgplay();
 //click sound function
 function clicksound(){
 let sound=new Audio('./Audio/click.mp3');
 sound.play();
 }
-//delay function
-function delay(){
-    setTimeout(function(){alert('hi')},1000)
-}
 //front screen
+//set kistners to play button
 const playgamebtn=document.getElementById('frontpagebtn');
 playgamebtn.addEventListener('click',playbtnclicked);
 function playbtnclicked(){
     clicksound();
-    delay();
-    console.log('hai')
+    setTimeout(function(){
+        frontscreen.style.display='none';
+        rulescreen.style.display='block';
+    },200)
 }
-document.getElementById('frontpage').style.display='block';
-document.getElementById('gamepage').style.display='block';
+//Game rules screen
+//setting rounds list
+const selector=document.getElementById('setrounds');
+for(var i=1;i<=10;i++){
+const options=document.createElement('option');
+options.appendChild(document.createTextNode(i));
+selector.appendChild(options);
+}
+//adding event lisners to the start button
+
