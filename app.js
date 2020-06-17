ab5a24111836af6f0101f37bbc713a3974f60bef
sarath <https://github.com/sarathrj10>
2020-06-17 10:42:13 +0530

    Update app.js

---
+++ app.js
@@ -0,0 +1,25 @@
+//backgroung music
+function bgplay(){
+    let bgsound=new Audio('./Audio/bgmusic.mp3');
+    bgsound.play();
+    }
+// bgplay();
+//click sound function
+function clicksound(){
+let sound=new Audio('./Audio/click.mp3');
+sound.play();
+}
+//delay function
+function delay(){
+    setTimeout(function(){alert('hi')},1000)
+}
+//front screen
+const playgamebtn=document.getElementById('frontpagebtn');
+playgamebtn.addEventListener('click',playbtnclicked);
+function playbtnclicked(){
+    clicksound();
+    delay();
+    console.log('hai')
+}
+// document.getElementById('frontpage').style.display='none';
+// document.getElementById('gamepage').style.display='block';
